ff4904dcc4c78597bbab376c9dcd88300b3176ff
Uliana313 <dzagrej@gmail.com>
2026-09-20 15:45:29 +0300

    Implement student B algorithm

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ std::unique_ptr<Result> calculateB(std::shared_ptr<const InputData> data);
 
 int main()
 {
-    std::cout << "Lab1_GroupProject: extremum search\n";
+    auto data = std::make_shared<const InputData>(InputData{
+        [](double x) { return (x - 2.0) * (x - 2.0) + 1.0; }, // f(x) = (x-2)^2 + 1, мінімум у x=2
+        0.0,   // a
+        5.0,   // b
+        1e-5   // eps
+    });
+
+    auto resultB = calculateB(data);
+    auto [xB, fxB, iterB, methodB] = *resultB;
+
+    std::cout << "Student B (" << methodB << "): x = " << xB
+               << ", f(x) = " << fxB << ", iterations = " << iterB << "\n";
+
     return 0;
 }
