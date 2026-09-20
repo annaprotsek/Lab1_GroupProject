9345df72fa02455d818b8da768bde25cb1e778d9
annaprotsek <annaprotsek@gmail.com>
2026-09-20 15:42:55 +0300

    Implement student A algorithm

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ std::unique_ptr<Result> calculateB(std::shared_ptr<const InputData> data);
 
 int main()
 {
-    std::cout << "Lab1_GroupProject: extremum search\n";
+    auto data = std::make_shared<const InputData>(InputData{
+        [](double x) { return (x - 2.0) * (x - 2.0) + 1.0; }, // f(x) = (x-2)^2 + 1, мінімум у x=2
+        0.0,   // a
+        5.0,   // b
+        1e-5   // eps
+    });
+
+    auto resultA = calculateA(data);
+    auto [xA, fxA, iterA, methodA] = *resultA;
+
+    std::cout << "Student A (" << methodA << "): x = " << xA
+               << ", f(x) = " << fxA << ", iterations = " << iterA << "\n";
+
     return 0;
 }
